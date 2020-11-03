1de6fb5949cea664eab5329cf42cdbd712861bcf
Colario <male7935@hotmail.com>
2020-11-03 10:31:16 +0100

    Base loop added

--- main.py
+++ main.py
@@ -2,6 +2,20 @@ import pygame
 pygame.init()
 
 
-run = True
-while run:
+import pygame
+
+pygame.init()
+
+
+finished = False
+clock = pygame.time.Clock()
+
+while not finished:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            finished = True
+
+
+    pygame.display.update()
+    clock.tick(60)
 
